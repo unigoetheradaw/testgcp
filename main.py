3e663a6dc31be02c13d704e642bc9f8a6bf9ebe5
Robert <Robert@10.108.26.166>
2018-07-06 13:40:31 +0200

    test3

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
+
+
 def main():
     print("Hallo World")
-    
+    print(os.environ.get('BUCKET_NAME',
+                         app_identity.get_default_gcs_bucket_name()))
     with open("sumfile.txt", "w") as f:
         f.write("Das ist ein Test")
     print(np.abs(-3.5))
+
 
 if __name__ == "__main__":
     main()
