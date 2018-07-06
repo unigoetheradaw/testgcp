5414bbfe2e1fa9d14bf8f79c09b233b8fe3eae76
Robert <Robert@10.108.26.166>
2018-07-06 13:58:23 +0200

    ysasd

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from google.appengine.api import app_identity
 import numpy as np
 
 def main():
-    print("Hallo World")
-    print(os.environ.get('BUCKET_NAME',
-                         app_identity.get_default_gcs_bucket_name()))
-    with open("sumfile.txt", "w") as f:
-        f.write("Das ist ein Test")
-    print(np.abs(-3.5))
-
+    datalist = "asdasd"
+    bucket_name = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
+    filename = os.path.join(bucket_name, 'myfile.txt')
+    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+    gcs_file = gcs.open(filename, 'w', content_type='text', retry_params=write_retry_params)
+    writer = csv.writer(gcs_file, delimiter='\t')
+    writer.writerow(['field1', 'field2'])
+    gcs_file.close()
 
 if __name__ == "__main__":
     main()
